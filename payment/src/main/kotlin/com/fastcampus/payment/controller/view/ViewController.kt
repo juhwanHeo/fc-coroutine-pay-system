596220edcf4a39e7a3ba7a54a1e7f116f6f12518
juhwanHeo <juhwanheo.dev@gmail.com>
2024-05-12 23:43:06 +0900

    feat: add view controller

--- payment/src/main/kotlin/com/fastcampus/payment/controller/view/ViewController.kt
+++ payment/src/main/kotlin/com/fastcampus/payment/controller/view/ViewController.kt
@@ -1,5 +1,7 @@
 package com.fastcampus.payment.controller.view
 
+import com.fastcampus.payment.controller.view.dto.ReqPayFailed
+import com.fastcampus.payment.controller.view.dto.ReqPaySucceed
 import com.fastcampus.payment.model.toResOrder
 import com.fastcampus.payment.service.OrderService
 import org.springframework.stereotype.Controller
@@ -22,5 +24,34 @@ class ViewController(
         model.addAttribute("pname", name)
         model.addAttribute("order", orderService.get(1).toResOrder())
         return "hello-world.html"
+    }
+
+    @GetMapping("/pay/{orderId}")
+    suspend fun pay(
+        @PathVariable("orderId") orderId: Long,
+        model: Model,
+    ): String {
+        val order = orderService.get(orderId)
+        model.addAttribute("order", order)
+        return "pay.html"
+    }
+
+    @GetMapping("/pay/success")
+    suspend fun successPay(
+        request: ReqPaySucceed,
+    ): String {
+        if (!orderService.authSucceed(request))
+            return "pay-fail.html"
+
+        orderService.capture(request)
+        return "pay-success.html"
+    }
+
+    @GetMapping("/pay/fail")
+    suspend fun failPay(
+        request: ReqPayFailed,
+    ): String {
+        orderService.authFailed(request)
+        return "pay-fail.html"
     }
 }
